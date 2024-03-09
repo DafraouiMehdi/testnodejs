8ce10a002bc6543d6506b3f90531ebe9ec160d17
Dafraouidev <dafraouimehdi05@gmail.com>
2024-03-09 22:17:58 +0100

    modified file

--- Apitest.js
+++ Apitest.js
@@ -9,6 +9,21 @@ app.get('/' , (req , responce)=>{
     responce.send('Get Req Successfully initited');
 })
 
+app.post('/post' , (req , responce)=>{
+    console.log('Get Request Successfully !')
+    responce.send('Get Req Successfully initited');
+})
+
+app.get('/put' , (req , responce)=>{
+    console.log('put Request Successfully !')
+    responce.send('put Req Successfully initited');
+})
+
+app.get('/delete' , (req , responce)=>{
+    console.log('delete Request Successfully !')
+    responce.send('delete Req Successfully initited');
+})
+
 app.listen(port , ()=>{
     console.log(`server estabilished at ${port}`);
 })
