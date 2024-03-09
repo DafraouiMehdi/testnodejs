fc19460d9db851e25f2935d0c45687daac24aaf5
Dafraouidev <dafraouimehdi05@gmail.com>
2024-03-09 23:09:43 +0100

    modified file Apitest.js

--- Apitest.js
+++ Apitest.js
@@ -2,17 +2,21 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.arguments(express.json());
+// app.arguments(express.json());
 
 app.get('/' , (req , responce)=>{
     console.log('Get Request Successfully !')
     responce.send('Get Req Successfully initited');
 })
 
-app.post('/post' , (req , responce)=>{
-    console.log('Get Request Successfully !')
-    responce.send('Get Req Successfully initited');
-})
+app.get('/post' , (req , responce)=>{
+    let username = 'dafraoui@gmail.com'; 
+    let password = 'Mehdi2005';
+    if (username === 'dafraouimehdi05@gmail.com' && password === 'Mehdi2005') {
+        responce.send('Welcome to page home');
+    }
+    console.log('Get Request Successfully !');
+});
 
 app.get('/put' , (req , responce)=>{
     console.log('put Request Successfully !')
